3db9b5452282b0ca92067dc6070c639a2940d43a
Annie Giang <annieg4123@gmail.com>
2019-10-02 09:53:01 -0700

    Find valid row that is clickable

--- src/board.js
+++ src/board.js
@@ -1,32 +1,37 @@
-// View
-
 import Tile from "./tile";
 
 const CONST = {
   WIDTH: 100,
   HEIGHT: 150,
   KEYS: ["d", "f", "j", "k"],
-  dY: 4
+  VELOCITY: 1
 };
 
 class Board {
-  constructor(rows, columns) {
+  constructor(dimentions, rows, columns) {
+    this.dimentions = dimentions;
     this.rows = rows;
     this.columns = columns;
-    this.grid = []; // 2d array of n rows * m columns
+    this.grid = []; // 2d array
+
+    // initialize rows to display at the start
+    for (let i = 0; i < this.rows; i++) {
+      this.grid.push(this.createRow(i));
+    }
   }
 
-  createRow(ctx, r) {
+  createRow(r = 0) {
     let row = [];
     let tile, tX, tY;
 
-    let targetIdx = Math.floor(Math.random() * this.columns);
+    let targetIdx = Math.floor(Math.random() * this.columns); // random target tile
 
     for (let i = 0; i < this.columns; i++) {
       tX = i * CONST.WIDTH;
-      tY = CONST.HEIGHT * r;
+      tY = r * CONST.HEIGHT; // calculate y position given r
+
       if (targetIdx === i) {
-        tile = new Tile(tX, tY, 1, CONST.KEYS[i]); // color = target color = 1
+        tile = new Tile(tX, tY, 1, CONST.KEYS[i]); //  target color = 1
       } else {
         tile = new Tile(tX, tY, 0, CONST.KEYS[i]);
       }
@@ -35,16 +40,7 @@ class Board {
     return row;
   }
 
-  createGrid(ctx) {
-    let row;
-    for (let i = 0; i < this.rows; i++) {
-      row = this.createRow(ctx, i);
-      this.grid.push(row);
-    }
-  }
-
-  renderGrid(ctx) {
-    this.createGrid(ctx);
+  drawGrid(ctx) {
     this.grid.forEach(row => {
       row.forEach(tile => {
         tile.drawTile(ctx);
@@ -55,15 +51,41 @@ class Board {
   moveRows() {
     this.grid.forEach(row => {
       row.forEach(tile => {
-        tile.y += CONST.dY;
+        // if the correct target in the current row is clicked
+        //  remove the borrom row (shift)
+        // create a new row (push)
+        // push all rows down by adding to the tile's height
+        // get current Row
       });
     });
   }
 
+  currentRow() {
+    this.currentRow = this.grid[0];
+  }
+
+  currentTargetPosition() {
+    this.currentRow.forEach(row => {
+      if (row.color === 1) {
+        return { x: row.x, y: row.y };
+      }
+    });
+  }
+
+  validBoundary() {
+    let minValidHeight = this.dimentions.height - CONST.HEIGHT;
+    let maxValidHeight = minValidHeight + CONST.HEIGHT;
+
+    let minValidWidth = 0;
+    let maxValidWidth = CONST.WIDTH;
+    return { minValidHeight, maxValidHeight, minValidWidth, maxValidWidth };
+  }
+
+  validTargetBoundary(mX, mY) {}
+
   animate(ctx) {
-    requestAnimationFrame(this.animate.bind(this));
-    // ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
     this.moveRows();
+    this.drawGrid(ctx);
   }
 }
 
--- src/game.js
+++ src/game.js
@@ -5,18 +5,44 @@ import Board from "./board";
 class Game {
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
-    this.board = new Board(4, 4);
+    this.dimentions = { width: canvas.width, height: canvas.height };
+    this.registerEvents();
+    this.restart();
   }
 
   animate() {
-    requestAnimationFrame(this.animate.bind(this));
-    // this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
     this.board.animate(this.ctx);
+
+    if (this.running) {
+      requestAnimationFrame(this.animate.bind(this));
+    }
   }
 
-  start() {
-    this.board.renderGrid(this.ctx);
-    requestAnimationFrame(this.animate.bind(this));
+  registerEvents() {
+    this.boundClickHandler = this.click.bind(this);
+    this.ctx.canvas.addEventListener("mousedown", this.boundClickHandler);
+  }
+
+  click(e) {
+    if (!this.running) {
+      // play if  not running & the mouse click is clicked on the target
+      this.play();
+      // console.log(this.board.validBoundary());
+      // console.log("x: ", e.offsetX);
+      // console.log("y: ", e.offsetY);
+    }
+  }
+
+  restart() {
+    this.running = false;
+    this.board = new Board(this.dimentions, 4, 4);
+    this.animate();
+  }
+
+  play() {
+    this.running = true;
+    this.animate();
   }
 }
 

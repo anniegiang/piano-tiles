4f40568c2ffbb845c2197c9a6f4db709b8b4d3aa
Annie Giang <annieg4123@gmail.com>
2019-10-06 22:15:33 -0700

    Create Music class

---
+++ src/music.js
@@ -0,0 +1,20 @@
+
+class Music(src) {
+  constructor(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.setAttribute("loop", "true");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+  }
+    play() {
+      this.sound.play();
+    };
+    stop() {
+      this.sound.pause();
+    };
+}
+
+export default Music;
